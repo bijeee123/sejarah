7e3864a370673511e52d12ca00405b604b6ade01
bijeee123 <abizarcahya559@gmail.com>
2025-03-15 14:52:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Animasi Scroll
+document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelectorAll('.timeline-item, .impact-item');
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    }, { threshold: 0.5 });
+  
+    items.forEach(item => {
+      observer.observe(item);
+    });
+  });
